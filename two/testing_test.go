84decf39b90d7b0493bfcf3d38bc2d086feb2b05
davodhiambo <jesseomolo@yahoo.com>
2024-12-02 22:39:55 +0300

    testing: add TestToInteger fix: fix separator at strings split.

---
+++ two/testing_test.go
@@ -0,0 +1,41 @@
+package two
+
+import "testing"
+
+func TestToInteger(t *testing.T) {
+	subject := []string{
+		"1 2 3 4 5 6 7 8 9 10",
+		"11 12 13 14 15 16 17 18 19 20",
+	}
+
+	expected := [][]int{
+		{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		{11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+	}
+
+	got := toInteger(subject)
+
+	if len(got) != len(expected) {
+		t.Errorf("Expected size %d, got %d", len(expected), len(got))
+		t.Errorf("TestToInteger Failed!")
+	}
+
+	var r1, r2, p1, p2 int
+	for r1 < len(got) && r2 < len(expected) {
+		if len(got[r1]) != len(expected[r2]) {
+			t.Errorf("Expected %v row at index %d to have a size of %d, got %v of size %d", expected[r2], r2, len(expected[r2]), got[r1], len(got[r1]))
+			t.Errorf("TestToInteger Failed!")
+		}
+		for p1 < len(got[r1]) && p2 < len(expected[r2]) {
+			if got[r1][p1] != expected[r2][p2] {
+				t.Errorf("Expected %d at index %d, %d at index %d to be equal", expected[r2][p2], p2, got[r1][p1], p1)
+				t.Errorf("TestToInteger Failed!")
+			}
+			p1++
+			p2++
+		}
+		r1++
+		r2++
+	}
+
+}
--- two/type_convert.go
+++ two/type_convert.go
@@ -9,8 +9,10 @@ func toInteger(reports []string) [][]int {
 	var result [][]int
 	var line []int
 
+	// Split trings using white space as separator to isolate numericals
+	// Convert to integer
 	for _, report := range reports {
-		nums := strings.Split(report, "")
+		nums := strings.Split(report, " ")
 
 		for _, num := range nums {
 			value, _ := strconv.Atoi(num)
