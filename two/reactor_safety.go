0a48a60a78221508eca396d7faaefdc64ec511ae
davodhiambo <jesseomolo@yahoo.com>
2024-12-02 23:09:52 +0300

    feat: add isValid

--- two/reactor_safety.go
+++ two/reactor_safety.go
@@ -3,3 +3,42 @@ package two
 func ascending(levels []int) bool {
 	return levels[0] < levels[len(levels)-1]
 }
+
+func isValid(levels []int) bool {
+	// Check for distubance in ascending or descending trends
+	// Check diffence of adjacent levels
+	if ascending(levels) {
+		p1 := 0
+		p2 := p1 + 1
+
+		for p1 < len(levels) && p2 < len(levels) {
+			if levels[p1] > levels[p2] {
+				return false
+			}
+
+			difference := levels[p2] - levels[p1]
+			if !(difference > 0 && difference < 4) {
+				return false
+			}
+			p1++
+			p2++
+		}
+	} else {
+		p1 := 0
+		p2 := p1 + 1
+
+		for p1 < len(levels) && p2 < len(levels) {
+			if levels[p1] < levels[p2] {
+				return false
+			}
+
+			difference := levels[p1] - levels[p2]
+			if !(difference > 0 && difference < 4) {
+				return false
+			}
+			p1++
+			p2++
+		}
+	}
+	return true
+}
