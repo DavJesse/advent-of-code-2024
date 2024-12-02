a54fb9259070d72eefd3b76ed881848cc9138dae
davodhiambo <jesseomolo@yahoo.com>
2024-12-02 19:55:10 +0300

    feat: add SimilarityScoreCalculator

--- two/frequency.go
+++ two/frequency.go
@@ -1,10 +1,35 @@
 package two
 
+import "advent-of-code/one"
+
 func FrequencyCalculator(nums []int) map[int]int {
 	result := make(map[int]int)
 
 	for _, num := range nums {
 		result[num]++
 	}
+	return result
+}
+
+func SimilarityScoreCalculator(list []int) int {
+	var frequencyScore int
+	var result int
+	left, right := one.Extract(list)
+
+	// Find frequency of integers in left and right lists
+	leftFreqMap := FrequencyCalculator(left)
+	rightFreqMap := FrequencyCalculator(right)
+
+	// Use keys in left list to find out if they exist in right list
+	// Calculate frequency score and accumulate in result
+	for key, valueLeft := range leftFreqMap {
+		valueRight, exists := rightFreqMap[key]
+
+		if exists {
+			frequencyScore = key * valueRight * valueLeft
+			result += frequencyScore
+		}
+	}
+
 	return result
 }
